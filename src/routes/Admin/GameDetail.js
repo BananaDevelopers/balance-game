86dab8cd3dd0880f3b17a3697904af93c951eda5
sejin <sejinnnnnn@naver.com>
2020-10-13 12:58:35 +0900

    gamedetail

--- src/routes/Admin/GameDetail.js
+++ src/routes/Admin/GameDetail.js
@@ -8,6 +8,7 @@ const GameDetail = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [id, setId] = useState("");
   const [title, setTitle] = useState("");
+  const [isAddQuiz, setIsAddQuiz] = useState(false);
   const [quizzes, setQuizzes] = useState([
     {
       id: 1111,
@@ -40,6 +41,32 @@ const GameDetail = () => {
       date: "2020-12-05",
     },
   ]);
+
+  const [quizTitle, setQuizTitle] = useState("");
+  const [QuizL, setQuizL] = useState("");
+  const [QuizR, setQuizR] = useState("");
+  
+  const onChange = (event) => {
+
+    const {
+      target: {value},
+    } = event;
+
+    switch(event.target.name) {
+      case "quizTitle":
+        setQuizTitle(value);
+        break;
+      case "QuizL":
+        setQuizL(value);
+        break;
+      case "QuizR":
+        setQuizR(value);
+        break;
+      default:
+        break;
+    }
+
+  }
 
   useEffect(() => {
     setId(params.id);
@@ -76,6 +103,16 @@ const GameDetail = () => {
     history.push(`/results/${id}`);
   };
 
+  const onAddQuizClick = () => {
+    setIsAddQuiz(true);
+  }
+
+  const onSubmit = async(event) => {
+    event.preventDefault();
+    const con = await dbService.collection("game").get(id);
+    console.log(con);
+  }
+
   return (
     <div>
       {isLoading ? (
@@ -89,8 +126,20 @@ const GameDetail = () => {
             <button onClick={onDeleteClick}>게임 삭제</button>
             <button onClick={onEditClick}>게임 편집</button>
             <button onClick={onResultClick}>결과 보기</button>
-            <button>퀴즈 추가</button>
+            <button onClick={onAddQuizClick}>퀴즈 추가</button>
           </div>
+          {isAddQuiz &&
+          <form onSubmit={onSubmit} style= {
+            {
+              display : "flex", flexDirection: "column", width: "200px",
+            }
+          }>
+            <input name="quizTitle" value={quizTitle} onChange={onChange} type="text" placeholder="퀴즈 이름" />
+            <input name="QuizL" value={QuizL} onChange={onChange} type="text" placeholder="왼쪽 지문" />
+            <input name="QuizR" value={QuizR} onChange={onChange} type="text" placeholder="오른쪽 지문" />
+            <input type="submit" value="추가" />
+          </form>
+          }
           <div>
             <p>퀴즈 목록</p>
             <ul>
