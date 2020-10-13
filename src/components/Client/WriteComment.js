f3fb57b19607ad4ef6646f8350d9915ecbf10009
hyunchocopie <h0625jj@gmail.com>
2020-10-13 14:26:35 +0900

    comments
    #1 comments

---
+++ src/components/Client/WriteComment.js
@@ -0,0 +1,88 @@
+import React, { useState, useRef } from "react";
+import { ADD_COMMENT } from "../../routes/Client/Gaming";
+import { dbService } from "fbase";
+
+const WriteComment = ({ dispatch, quizObj, propleft }) => {
+  const [nickname, setNickname] = useState("");
+  const [pw, setPW] = useState("");
+  const [comment, setComment] = useState("");
+
+  console.log(propleft);
+  const onChange = e => {
+    const { target: { value } } = e;
+    const { target: { name } } = e;
+
+    switch (name) {
+      case "nickname":
+        setNickname(value);
+        break;
+      case "pw":
+        setPW(value);
+        break;
+      case "comment":
+        setComment(value);
+        break;
+    }
+  };
+
+  const addDB = async cmtObj => {
+    let cmtDocId;
+
+    await dbService
+      .collection("comment")
+      .add(cmtObj)
+      .then(res => {
+        cmtDocId = res.id;
+        console.log(cmtDocId);
+      });
+
+    await dbService.doc(`quiz/${quizObj.docid}`).update({
+      comments: [cmtDocId, ...quizObj.comments]
+    });
+
+    dispatch({
+      type: ADD_COMMENT,
+      cmtObj: cmtDocId,
+      qdocid: quizObj.docid
+    });
+  };
+
+  const onSubmit = () => {
+    const cmtObj = {
+      description: comment,
+      date: Date().toLocaleString(),
+      nickname: nickname,
+      password: pw,
+      like: 0,
+      left: propleft,
+      reply: []
+    };
+
+    addDB(cmtObj);
+    setComment("");
+    setNickname("");
+    setPW("");
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <div>
+          <span>nickname</span>
+          <input
+            type="text"
+            name="nickname"
+            value={nickname}
+            onChange={onChange}
+          />
+          <span>password</span>
+          <input type="password" name="pw" value={pw} onChange={onChange} />
+        </div>
+        <input type="text" name="comment" value={comment} onChange={onChange} />
+        <input type="submit" value="입력" />
+      </form>
+    </>
+  );
+};
+
+export default WriteComment;
