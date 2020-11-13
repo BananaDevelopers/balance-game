f2b3b21ac80443ac9cfef6aedbe6b0ee01820ca9
Lee Hyun Se <0409hshs@gmail.com>
2020-11-13 17:25:32 +0900

    GameStart Page css
    css with StyledComponent
    and replace Progressabar with new libraray

--- src/components/Client/Choice.js
+++ src/components/Client/Choice.js
@@ -1,7 +1,14 @@
 import React from "react";
+import styled from "styled-components";
+
+const QuizText = styled.div`
+  width: 11vw;
+  line-height: 5vh;
+  font-size: 18px;
+`;
 
 const Choice = ({ text }) => {
-  return <div className="choice">{text}</div>;
+  return <QuizText>{text}</QuizText>;
 };
 
 export default Choice;
